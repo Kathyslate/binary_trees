b780571f4d8f4d8d2d65be251e84321645808699
larryacademy001 <larryacademy@gmail.com>
2024-03-28 05:00:17 +0100

    Update 100-main.c

--- 100-main.c
+++ 100-main.c
@@ -28,21 +28,21 @@ void launch_test(binary_tree_t *n1, binary_tree_t *n2)
  */
 int main(void)
 {
-    binary_tree_t *root;
+	binary_tree_t *root;
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    root->left->right = binary_tree_node(root->left, 54);
-    root->right->right = binary_tree_node(root->right, 128);
-    root->left->left = binary_tree_node(root->left, 10);
-    root->right->left = binary_tree_node(root->right, 45);
-    root->right->right->left = binary_tree_node(root->right->right, 92);
-    root->right->right->right = binary_tree_node(root->right->right, 65);
-    binary_tree_print(root);
+	root = binary_tree_node(NULL, 98);
+	root->left = binary_tree_node(root, 12);
+	root->right = binary_tree_node(root, 402);
+	root->left->right = binary_tree_node(root->left, 54);
+	root->right->right = binary_tree_node(root->right, 128);
+	root->left->left = binary_tree_node(root->left, 10);
+	root->right->left = binary_tree_node(root->right, 45);
+	root->right->right->left = binary_tree_node(root->right->right, 92);
+	root->right->right->right = binary_tree_node(root->right->right, 65);
+	binary_tree_print(root);
 
-    launch_test(root->left, root->right);
-    launch_test(root->right->left, root->right->right->right);
-    launch_test(root->right->right, root->right->right->right);
-    return (0);
+	launch_test(root->left, root->right);
+	launch_test(root->right->left, root->right->right->right);
+	launch_test(root->right->right, root->right->right->right);
+	return (0);
 }
